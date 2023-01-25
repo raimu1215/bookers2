4934ecc5600a522fa670bc13460f19751c18ce7c
EC2 Default User <ec2-user@ip-172-31-1-172.ap-northeast-1.compute.internal>
2023-01-25 08:17:43 +0000

    [テスト]途中

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -28,13 +28,14 @@ class BooksController < ApplicationController
   end
   
   def create
-    @book = Book.new(book_params)
-    @book.user_id = current_user.id
-    if @book.save
+    @newbook = Book.new(book_params)
+    @newbook.user_id = current_user.id
+    if @newbook.save
       flash[:notice] = "You have created book successfully."
-      redirect_to book_path(@book.id)
+      redirect_to book_path(@newbook.id)
     else
       @users = current_user
+      @books = Book.all
       render :index
     end
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -32,10 +32,10 @@ class UsersController < ApplicationController
   end
   
   def update
-    @user = current_user
+    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = 'You have updated user successfully.'
-      redirect_to books_path
+      redirect_to user_path(@user.id)
     else
       render :edit
     end
